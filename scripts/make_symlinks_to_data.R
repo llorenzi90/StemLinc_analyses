d9bec15d6237e43537ae35aad235ddb5545bee06
Lucia Lorenzi <lucialorenzi90@gmail.com>
2024-09-16 17:25:49 +0200

    updated script to make symlinks to data

--- scripts/make_symlinks_to_data.R
+++ scripts/make_symlinks_to_data.R
@@ -1,15 +1,19 @@
-list_render_params=list(Kli_MarkedDups_guided =
-                          list(tracking_path = '/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/public_datasets/analyses/gffcompare_Kli_MarkedDups_guided/Kli_MarkedDups_guided.tracking',
-                               gtf_path= '/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/public_datasets/analyses/gffcompare_Kli_MarkedDups_guided/Kli_MarkedDups_guided.combined.gtf'),
+# list_render_params=list(Kli_MarkedDups_guided =
+#                           list(tracking_path = '/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/public_datasets/analyses/gffcompare_Kli_MarkedDups_guided/Kli_MarkedDups_guided.tracking',
+#                                gtf_path= '/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/public_datasets/analyses/gffcompare_Kli_MarkedDups_guided/Kli_MarkedDups_guided.combined.gtf'),
+#
+#                         LSK_StemLinc.guided =
+#                           list(tracking_path= '/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/Experimento4_LSK_190224/analyses/strtie_transcriptome_assembly/gffcompare/LSK_StemLinc.tracking' ,
+#                                gtf_path ='/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/Experimento4_LSK_190224/analyses/strtie_transcriptome_assembly/gffcompare/LSK_StemLinc.combined.gtf' ),
+#
+#                         LSK_StemLinc.blind =
+#                           list(tracking_path="/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/Experimento4_LSK_190224/analyses/strtie_transcriptome_assembly/gffcompare_blind/LSK_StemLinc.blind.tracking",
+#                                gtf_path="/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/Experimento4_LSK_190224/analyses/strtie_transcriptome_assembly/gffcompare_blind/LSK_StemLinc.blind.combined.gtf"))
+# list_render_params
+#
+# linkdir="data/raw/"
+# lapply(list_render_params,function(fi)sapply(paste0("ln -s '",fi,"' ",linkdir),function(cm)system(cm)))
 
-                        LSK_StemLinc.guided =
-                          list(tracking_path= '/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/Experimento4_LSK_190224/analyses/strtie_transcriptome_assembly/gffcompare/LSK_StemLinc.tracking' ,
-                               gtf_path ='/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/Experimento4_LSK_190224/analyses/strtie_transcriptome_assembly/gffcompare/LSK_StemLinc.combined.gtf' ),
-
-                        LSK_StemLinc.blind =
-                          list(tracking_path="/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/Experimento4_LSK_190224/analyses/strtie_transcriptome_assembly/gffcompare_blind/LSK_StemLinc.blind.tracking",
-                               gtf_path="/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/Lucia/Marie_Curie/Experimento4_LSK_190224/analyses/strtie_transcriptome_assembly/gffcompare_blind/LSK_StemLinc.blind.combined.gtf"))
-list_render_params
-
-linkdir="data/raw/"
-lapply(list_render_params,function(fi)sapply(paste0("ln -s '",fi,"' ",linkdir),function(cm)system(cm)))
+linkdir="data/references/merged_refs_annotation/"
+ref_gtf='/run/user/1608803857/gvfs/smb-share:server=10.110.20.13,share=investigacio/Cuartero Group/CUARTERO GROUP/references/mouse/annotation/merged_refs_annotation/merged_refs.combined.annot_trnames.gtf'
+system(paste0("ln -s '",ref_gtf,"' ",linkdir))
