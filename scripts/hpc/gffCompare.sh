5d81b601fb4f90792e9581892aee36a4322bd8a0
Lucia Lorenzi <lucialorenzi90@gmail.com>
2025-01-14 11:52:07 +0100

    generalize gffCompare for any gtf list

--- scripts/hpc/gffCompare.sh
+++ scripts/hpc/gffCompare.sh
@@ -10,10 +10,11 @@ module load GffCompare/0.12.6-GCC-11.2.0
 refgtf="/mnt/beegfs/llorenzi/jobs/StemLinc/gffcompare_merge_ref_annotations/merged_refs.combined.gtf"
 smaskedgenome="/mnt/beegfs/public/references/genome/mouse/soft_masked/mm39.fa"
 
-outpref=$1 #LSK_StemLinc
+query_gtfs_file=$1 # strtie_guided_gtfs.txt
+outpref=$2 #LSK_StemLinc
 
 mkdir gffcompare
 
-gffcompare -i strtie_guided_gtfs.txt -r $refgtf -s $smaskedgenome -V -j gffcompare/$outpref.new_junction.tsv -o gffcompare/$outpref
+gffcompare -i $query_gtfs_file -r $refgtf -s $smaskedgenome -V -j gffcompare/$outpref.new_junction.tsv -o gffcompare/$outpref
 
 
--- scripts/hpc/gffCompare_blind.sh
+++ scripts/hpc/gffCompare_blind.sh
@@ -10,12 +10,12 @@ module load GffCompare/0.12.6-GCC-11.2.0
 refgtf="/mnt/beegfs/llorenzi/jobs/StemLinc/gffcompare_merge_ref_annotations/merged_refs.combined.gtf"
 smaskedgenome="/mnt/beegfs/public/references/genome/mouse/soft_masked/mm39.fa"
 
-outpref=$1 #LSK_StemLinc.blind
+query_gtfs_file=$1 # strtie_blind_gtfs.txt
+outpref=$2 # LSK_StemLinc.blind
 
 mkdir gffcompare_blind
 
-gffcompare -i strtie_blind_gtfs.txt -r $refgtf -s $smaskedgenome -V -j gffcompare_blind/$outpref.new_junction.tsv -o gffcompare_blind/$outpref
-
+gffcompare -i $query_gtfs_file -r $refgtf -s $smaskedgenome -V -j gffcompare_blind/$outpref.new_junction.tsv -o gffcompare_blind/$outpref
 
 gffcompare -r $refgtf -s $smaskedgenome -V -j gffcompare_blind/combined_$outpref.new_junction.tsv -o gffcompare_blind/combined_$outpref gffcompare_blind/$outpref.combined.gtf
 
